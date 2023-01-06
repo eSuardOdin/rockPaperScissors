7e5c57524d1e7980c6fb57417ca7c75e8205cd79
eSuardOdin <esuardodin@gmail.com>
2023-01-06 22:25:37 +0100

    Starting working on style, added a return player value

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+const getPlayerValue = () => {
+    return document.querySelector('input[name="rps"]:checked').getAttribute('value');
+}
+
+
 const getComputerChoice = () => {
+    getPlayerValue();
     if (Math.floor(Math.random() * 3) === 0){
         return "Rock";    
     } else if (Math.floor(Math.random() * 3) === 1) {
@@ -6,3 +12,4 @@ const getComputerChoice = () => {
     }
     return "Scissors";
 }
+
