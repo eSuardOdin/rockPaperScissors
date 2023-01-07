4be325899b10a391d725b359f908461fe7fb155b
eSuardOdin <esuardodin@gmail.com>
2023-01-07 09:53:43 +0100

    Game playable

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const getPlayerValue = () => {
+// Returns the player's choice as string
+const getPlayerChoice = () => {
     return document.querySelector('input[name="rps"]:checked').getAttribute('value');
 }
 
 
+// Returns the computer choice as string
 const getComputerChoice = () => {
-    getPlayerValue();
+    getPlayerChoice();
     if (Math.floor(Math.random() * 3) === 0){
         return "Rock";    
     } else if (Math.floor(Math.random() * 3) === 1) {
@@ -22,4 +24,49 @@ const turnOnBtn = () => {
     }));
 }
 
+// Turn on button calls
 turnOnBtn();
+
+
+// Update the score on the index.html
+const updateScore = (res) => {
+    const humanScore = document.querySelector('#hum-score');
+    const computerScore = document.querySelector('#com-score');
+    let humanScoreNb = Number(document.querySelector('#hum-score').innerHTML);
+    let computerScoreNb = Number(document.querySelector('#com-score').innerHTML);
+    // If computer wins
+    if (res < 0) {
+        computerScoreNb += Math.abs(res);
+    } else {
+        humanScoreNb += res;
+    }
+    humanScore.innerHTML = humanScoreNb;
+    computerScore.innerHTML = computerScoreNb;
+}
+
+const computeWinner = (huM, coM) => {
+    if( // Win
+        huM === "Rock" && coM === "Scissors" ||
+        huM === "Scissors" && coM === "Paper" ||
+        huM === "Paper" && coM === "Rock") {
+            return 1;
+    } else if( //Lose
+        coM === "Rock" && huM === "Scissors" ||
+        coM === "Scissors" && huM === "Paper" ||
+        coM === "Paper" && huM === "Rock") {
+            return -1;
+        }
+    return 0; //Draw
+}
+
+
+const playRound = () => {
+    const playerMove = getPlayerChoice();
+    const comMove = getComputerChoice();
+    console.log(`
+    Player : ${playerMove}
+    Computer : ${comMove}
+    `);
+
+    updateScore(computeWinner(playerMove, comMove));
+}
